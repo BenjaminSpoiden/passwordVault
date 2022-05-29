c44f03ef0e58ce40a729040b1eb05e879dd43b94
benjaminSpoiden <ben@Ben-PC.localdomain>
2022-05-29 13:59:29 +0200

    prisma setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,35 @@
+import { logger } from "./utils/logger"
 import { CreateServer } from "./utils/createServer"
 import { handler } from "./utils/promiseHandler"
+import { FastifyInstance } from "fastify"
+import { prismaClient } from "./utils/db"
 
-console.log("INDEX FILE")
-
+const shutdown = (shutdownSignal: string, app: FastifyInstance) => {
+    process.on(shutdownSignal, async () => {
+        logger.info('Shutting Down Server')
+        app.close()
+        prismaClient.$disconnect()
+        process.exit(0)
+    })
+}
 
 const main = async () => {
     const server = CreateServer()
 
     const { data, error } = await handler(server.listen(4000, '0.0.0.0'))
     if(error) { 
-        console.error(error)
+        logger.error(error)
         process.exit(1) 
     }
 
-    console.log(`Server Listening at ${data}`)
+    logger.info(`Server Listening at ${data}`)
+
+    const shutdownSignals = ["SIGTERM", "SIGINT"]
+
+    shutdownSignals.forEach(signal => {
+        shutdown(signal, server)
+    })
+
     
 }
 
